593d181ccd39f2b42f38691a077bcc09bc17170f
l-k-mist <dylan@vandenbosch.co.za>
2022-02-22 09:17:25 +0200

    experiment(trs timing): tested

--- src/App.vue
+++ src/App.vue
@@ -40,21 +40,20 @@
             url="https://cs.stratumfive.com/tile/256/{z}/{y}/{x}/png?layers=cmap"
           />
         </ol-tile-layer>
-        <!-- <ol-tile-layer :opacity="0.3">
-          <ol-source-xyz
-            ref="weatherSource"
-            url="https://cs.stratumfive.com/tile/256/{z}/{y}/{x}/png?layers=bg:000;land:021;density:_all:2021-01"
-          />
-        </ol-tile-layer> -->
+        <ol-tile-layer :opacity="1">
+          <ol-source-xyz ref="weatherSource" />
+        </ol-tile-layer>
       </ol-map>
     </div>
+    <trs-date-incrementor v-model="trsDate"></trs-date-incrementor>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, onMounted, computed, watch } from "vue";
 
 import { OlMap } from "vue3-openlayers";
+import TileGrid from "ol/tilegrid/TileGrid";
 import "vue3-openlayers/dist/vue3-openlayers.css";
 // import SMap from "@/BorrowedFromBaseplate/SMap.vue";
 // import HelloWorld from "./components/HelloWorld.vue";
@@ -64,29 +63,55 @@ import { useStorage } from "@vueuse/core";
 // Not too much, that's what the cache is for,
 // but some. Start with numbers like 30ms to 100ms.
 import { getAll, putTile, destroy } from "@/api/api-pouch";
-import { useThrottle } from "@vueuse/core";
-
+// import { useThrottle } from "@vueuse/core";
+import TrsDateIncrementor from "@/components/TrsDateIncrementor.vue";
+const BASE_URL = "https://cs.stratumfive.com/tile/256/{z}/{y}/{x}/png";
 export default defineComponent({
   name: "App",
   components: {
     OlMap,
+    TrsDateIncrementor,
   },
   setup() {
     const center = ref([40, 40]);
     const projection = ref("EPSG:4326");
-    const zoom = ref(8);
+    const zoom = ref(1);
     const rotation = ref(0);
     const source = ref(null);
     const weatherSource = ref(null);
+    const map = ref(null);
     const mapHeight = ref(700);
     // const vesselMap = ref(null);
+    const trsDate = ref(2021102505);
+    // trs2:2021102617::::
+    // "?layers=trs:15bd173d:9400D3:0.5:2:"
+    const trsQuery = computed(() => `?layers=trs2:${trsDate.value}::::`); // without the date at the end like 2021102514
+    const trsUrl = computed(() => `${BASE_URL}${trsQuery.value}`);
 
     onMounted(() => {
-      source.value.source.tileLoadFunction = tileLoadFunctionTerrain;
-      // weatherSource.value.source.tileLoadFunction = tileLoadFunctionWeather;
-      console.log("dvdb - onMounted - source", source.value);
+      source.value.source.setTileLoadFunction(tileLoadFunctionTerrain);
+      weatherSource.value.source.setTileLoadFunction(tileLoadFunctionWeather);
+      weatherSource.value.source.setUrl(trsUrl.value);
+      // weatherSource.value.source.tileSize = 512;
+      // weatherSource.value.source.tmpSize = [512, 512];
+      // weatherSource.value.source.tilePixelRatio_ = 2;
+      console.log(
+        "dvdb - onMounted -  weatherSource.value.source",
+        weatherSource.value.source
+      );
+      // console.log("dvdb - onMounted - map.value.map", map.value);
+      // const tileGrid = new TileGrid({
+      //   minZoom: 6,
+      //   extent: map.value.map.getExtent(),
+      //   resolutions: map.value.map.getResolutions().slice(1),
+      //   tileSize: [512, 512],
+      // });
+      // weatherSource.value.source.setTileGrid(tileGrid);
     });
-
+    watch(trsUrl, async (newUrl) => {
+      console.log("dvdb - watch - newUrl", newUrl);
+      weatherSource.value.source.setUrl(newUrl);
+    });
     // CACHING
     onMounted(() => {
       initialiseCache();
@@ -95,6 +120,10 @@ export default defineComponent({
     const cacheVersion = 1;
     const cacheName = `map-terrain-${cacheVersion}`;
     const cache = ref();
+
+    // function sleep(ms) {
+    //   new Promise((resolve) => setTimeout(resolve, ms));
+    // }
     async function initialiseCache() {
       cache.value = await caches.open(cacheName);
       const cacheAvailable = "caches" in window;
@@ -112,33 +141,6 @@ export default defineComponent({
         estimate.usage ? estimate.usage / 1000000 : "can't say"
       );
     }
-
-    function tileLoadFunctionTerrain(tile, url) {
-      onTileLoad(tile, url);
-      // tile.getImage().src = src;
-    }
-    // Tile Loading Starts
-    // const input = ref("");
-
-    // function debouncedTileLoadFunctionTerrain(tile, src) {
-    //   return useThrottle(
-    //     ref(() => {
-    //       console.log("dvdb - returnuseThrottle - useThrottle");
-    //       tileLoadFunctionTerrain(tile, src);
-    //     }),
-    //     5000
-    //   );
-    // }
-    // const updated = ref(0);
-    // watch(debounced, () => (updated.value += 1));
-
-    function tileLoadFunctionWeather(tile, url) {
-      onTileLoad(tile, url);
-      // tile.getImage().src = url;
-    }
-    // function sleep(ms) {
-    //   new Promise((resolve) => setTimeout(resolve, ms));
-    // }
     async function deleteCache() {
       const deleteSuccess = await caches.delete(cacheName);
       destroy();
@@ -150,7 +152,7 @@ export default defineComponent({
       console.log("dvdb - logCache - response", response);
       findImages();
     }
-
+    // END OF CACHING Aim for useFunction
     async function findImages() {
       // Get a list of all of the caches for this origin
       const cacheNames = await cache.value.keys();
@@ -173,6 +175,16 @@ export default defineComponent({
       return result;
     }
 
+    // TILE LOADING USING CACHE Turn to useFunction
+    function tileLoadFunctionTerrain(tile, url) {
+      onTileLoad(tile, url);
+      // tile.getImage().src = src;
+    }
+
+    function tileLoadFunctionWeather(tile, url) {
+      onTileLoad(tile, url);
+      // tile.getImage().src = url;
+    }
     async function displayTileFromCache(tile, response) {
       const newBlob = await response.blob();
       // eslint-disable-next-line no-param-reassign
@@ -232,6 +244,9 @@ export default defineComponent({
       tileLoadFunctionWeather,
       deleteCache,
       logCache,
+      trsUrl,
+      trsDate,
+      map,
     };
   },
 });
---
+++ src/components/TrsDateIncrementor.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bg-white shadow sm:rounded-lg">
+    <div class="px-4 py-5 sm:p-6">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">
+        TRS Date Cycler
+      </h3>
+      <div class="mt-2 max-w-xl text-sm text-gray-500">
+        <p>Increment between 202110250202 and 202110250214</p>
+        <p>Expect Malou to jump at 202110250203, for correct behaviour.</p>
+      </div>
+      <form class="mt-5 sm:flex sm:items-center">
+        <div class="w-full sm:max-w-xs">
+          <label for="email" class="sr-only">Email</label>
+          <input
+            type="number"
+            name="dateNumber"
+            id="dateNumber"
+            v-model="value"
+            @input="$emit('input', value)"
+            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: Number,
+    },
+  },
+  setup(props, { emit }) {
+    const value = computed({
+      get: () => props.modelValue,
+      set: (value) => emit("update:modelValue", value),
+    });
+
+    return { value };
+  },
+});
+</script>
+
+<style scoped></style>
